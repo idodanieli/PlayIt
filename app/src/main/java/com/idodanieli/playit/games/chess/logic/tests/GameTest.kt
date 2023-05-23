47645361a2b418377c3d04696ac86322ad5802c9
Ido Danieli <idodanieli@gmail.com>
2023-05-23 19:47:48 +0300

    add canMove tests

--- app/src/main/java/com/idodanieli/playit/games/chess/logic/tests/GameTest.kt
+++ app/src/main/java/com/idodanieli/playit/games/chess/logic/tests/GameTest.kt
@@ -22,8 +22,18 @@ class GameTest {
         assert(game.board.pieceAt(destination) == pawn)
         assert(enemyPawn !in game.board.whitePieces)
 
-        assert(origin !in game.board.map) { errorFormat(game.board, "origin square $origin still in board.map even after move") }
-        assert(destination in game.board.map) { errorFormat(game.board, "dest square $destination not in board.map even after move") }
+        assert(origin !in game.board.map) {
+            errorFormat(
+                game.board,
+                "origin square $origin still in board.map even after move"
+            )
+        }
+        assert(destination in game.board.map) {
+            errorFormat(
+                game.board,
+                "dest square $destination not in board.map even after move"
+            )
+        }
     }
 
     @Test
@@ -44,7 +54,12 @@ class GameTest {
         val finishedGame = Game(TEST_GAME_NAME, mutableSetOf(bKing, bPawn, wQueen), CHESSBOARD_SIZE)
         finishedGame.currentPlayer = Player.BLACK
 
-        assert(finishedGame.isOver()) { errorFormat(finishedGame.board, "test returned that the game wasn't over even though it was ( queen checkmates king )") }
+        assert(finishedGame.isOver()) {
+            errorFormat(
+                finishedGame.board,
+                "test returned that the game wasn't over even though it was ( queen checkmates king )"
+            )
+        }
 
         // Game isn't over
         // k . . . . . . .
@@ -55,35 +70,48 @@ class GameTest {
         // . . . . . . . .
         // . . . . . . . .
         // Q . . . . . . .
-        val unthreateningWQueen = Queen(Square(0 , 7), Player.WHITE)
-        val unfinishedGame = Game(TEST_GAME_NAME, mutableSetOf(bKing, bPawn, unthreateningWQueen), CHESSBOARD_SIZE)
+        val unthreateningWQueen = Queen(Square(0, 7), Player.WHITE)
+        val unfinishedGame =
+            Game(TEST_GAME_NAME, mutableSetOf(bKing, bPawn, unthreateningWQueen), CHESSBOARD_SIZE)
         unfinishedGame.currentPlayer = Player.BLACK
 
-        assert(!unfinishedGame.isOver()) { errorFormat(unfinishedGame.board, "test returned that the game was over even though it wasn't") }
+        assert(!unfinishedGame.isOver()) {
+            errorFormat(
+                unfinishedGame.board,
+                "test returned that the game was over even though it wasn't"
+            )
+        }
     }
+
+    // These are used for testFilterBlockingMoves and testCanMove
+    private val bBishopOriginSquare = Square(2, 0)
+
+    private val bKing = King(Square(0, 0), Player.BLACK)
+    private val bBishop = Bishop(bBishopOriginSquare, Player.BLACK)
+    private val bPawn = Pawn(Square(6, 1), Player.BLACK)
+    private val wQueen = Queen(Square(2, 2), Player.WHITE)
+
+    // Bishop could move to (1, 1) to block the check
+    // k . b . . . . .
+    // . . . . . . p .
+    // . . Q . . . . .
+    // . . . . . . . .
+    // . . . . . . . .
+    // . . . . . . . .
+    // . . . . . . . .
+    // . . . . . . . .
+    private val game =
+        Game(TEST_GAME_NAME, mutableSetOf(bKing, bBishop, bPawn, wQueen), CHESSBOARD_SIZE)
 
     @Test
     fun testFilterBlockingMoves() {
-        val bBishopOriginSquare = Square(2, 0)
-
-        val bKing = King(Square(0,0 ), Player.BLACK)
-        val bBishop = Bishop(bBishopOriginSquare, Player.BLACK)
-        val wQueen = Queen(Square(2, 2), Player.WHITE)
-
-        // Bishop could move to (1, 1) to block the check
-        // k . b . . . . .
-        // . . . . . . . .
-        // . . Q . . . . .
-        // . . . . . . . .
-        // . . . . . . . .
-        // . . . . . . . .
-        // . . . . . . . .
-        // . . . . . . . .
-        val game = Game(TEST_GAME_NAME, mutableSetOf(bKing, bBishop, wQueen), CHESSBOARD_SIZE)
-        val blockingMove = Square(1, 1)
-
-        assert(blockingMove in game.filterBlockingMoves(bBishop, bBishop.validMoves(game.board)))
-        { errorFormat(game.board, "test result concluded that $bBishop couldn't block the check even though it could") }
+        assert(game.filterBlockingMoves(bBishop, bBishop.validMoves(game.board)).size == 1)
+        {
+            errorFormat(
+                game.board,
+                "test result concluded that $bBishop couldn't block the check even though it could"
+            )
+        }
 
         assert(bBishop.square == bBishopOriginSquare) { "bishops origin square change after test from: $bBishopOriginSquare to ${bBishop.square}" }
 
@@ -97,11 +125,35 @@ class GameTest {
         // . . . . . . . .
         // . . . . . . . .
         val wRook = Rook(Square(0, 1), Player.WHITE)
-        val game2 = Game(TEST_GAME_NAME, mutableSetOf(bKing, bBishop, wQueen, wRook), CHESSBOARD_SIZE)
+        val game2 =
+            Game(TEST_GAME_NAME, mutableSetOf(bKing, bBishop, wQueen, wRook), CHESSBOARD_SIZE)
         val blockingMoves = game2.filterBlockingMoves(bBishop, bBishop.validMoves(game2.board))
 
         assert(blockingMoves.isEmpty())
-        { errorFormat(game2.board, "test result concluded that $blockingMoves could block the check even though it couldn't") }
+        {
+            errorFormat(
+                game2.board,
+                "test result concluded that $blockingMoves could block the check even though it couldn't"
+            )
+        }
+    }
+
+    @Test
+    fun testCanMove() {
+        game.currentPlayer = Player.BLACK
+
+        assert(!game.canMove(bPawn.square, Square(6, 2))) {
+            errorFormat(
+                game.board,
+                "$bPawn could move even though the king is in check"
+            )
+        }
+        assert(game.canMove(bBishop.square, Square(1, 1))) {
+            errorFormat(
+                game.board,
+                "$bBishop couldn't move even though it could block the check"
+            )
+        }
     }
 
     companion object {
