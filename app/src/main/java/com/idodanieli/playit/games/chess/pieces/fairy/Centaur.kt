7a2f448d9b565b3bab901d25d3c258219dface68
Ido Danieli <idodanieli@gmail.com>
2023-06-25 23:35:42 +0300

    Implement Centaur = Man + Knight

--- app/src/main/java/com/idodanieli/playit/games/chess/pieces/fairy/Centaur.kt
+++ app/src/main/java/com/idodanieli/playit/games/chess/pieces/fairy/Centaur.kt
@@ -1,22 +1,20 @@
 package com.idodanieli.playit.games.chess.pieces.fairy
 
-import com.idodanieli.playit.games.chess.logic.Board
 import com.idodanieli.playit.games.chess.logic.Player
 import com.idodanieli.playit.games.chess.logic.Square
-import com.idodanieli.playit.games.chess.pieces.BasePiece
 import com.idodanieli.playit.games.chess.pieces.Piece
-import com.idodanieli.playit.games.chess.pieces.classic.King
 import com.idodanieli.playit.games.chess.pieces.classic.Knight
+import com.idodanieli.playit.games.chess.pieces.core.Compound
 
 
 const val TYPE_CENTAUR = "S"
 
-class Centaur(square: Square, player: Player) : BasePiece(square, player) {
+class Centaur(square: Square, player: Player) : Compound(square, player) {
     override val type = TYPE_CENTAUR
-
-    override fun availableSquares(board: Board): List<Square> {
-        return Knight(square, player).availableSquares(board) + board.neighborSquares(this)
-    }
+    override val pieces = listOf(
+        Knight(square, player),
+        Man(square, player)
+    )
 
     // --- General ---------------------------------------------------------------------------------
     override fun copy(): Piece {
